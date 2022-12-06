963f6734e1e231d76de214d06894044184e6a358
Glory Sinkhonde <sinkhondeglory@gmail.com>
2022-12-06 20:22:38 +0200

    New Project

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <nav>
+        <ul>
+          <li>Shop</li>
+          <li>Home</li>
+          <li>About</li>
+          <li>Shop</li>
+          <li>Shop</li>
+          <li>Shop</li>
+          <li>Shop</li>
+        </ul>
+      </nav>
+    </div>
+  )
+}
+
+export default App
